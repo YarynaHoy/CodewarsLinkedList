f0efad5a09eb29615d6610d1a7051aa5234faf3e
Yaryna <yarynka.hoy@gmail.com>
2026-03-22 16:13:13 +0200

    add index.py

---
+++ Linked-Lists-Get-Nth-Node/inex.py
@@ -0,0 +1,17 @@
+class Node(object):
+    """Node class for reference"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def get_nth(node, index):
+    current = node
+    current_index = 0
+
+    while current is not None:
+        if current_index == index:
+            return current
+        current = current.next
+        current_index += 1
+
+    raise Exception('index out of range')
